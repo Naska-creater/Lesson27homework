cd35bafe930583ecf9902026ce7232e31d56a31b
Nastya <Naska6997224@mail.ru>
2025-04-02 20:28:16 +0300

    Доработка формы

--- myproject4/task3/urls.py
+++ myproject4/task3/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import input_view
+from .views import UserInputFormView
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', input_view, name='input_form'),
+    path('form/', UserInputFormView.as_view(), name='form'),
     path('success/', TemplateView.as_view(template_name='task3/success.html'), name='success'),
 ]
 
--- myproject4/task3/views.py
+++ myproject4/task3/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
+from django.views import View
+
 from .forms import UserInputForm
 
-def input_view(request):
-    if request.method == 'POST':
-        form = UserInputForm(request.POST)
-        if form.is_valid():
-            form.save()  # Сохранение данных в базе данных
-            return redirect('success')  # Перенаправление на страницу успешной отправки
-    else:
-        form = UserInputForm()
-    return render(request, 'task3/input_form.html', {'form': form})
+class UserInputFormView(View):
 
+    def get (self, request, *args, **kwargs):
+            form = UserInputForm()
+            return render(request, 'task3/input_form.html', {'form': form})
+
+    def post (self, request, *args, **kwargs):
+            form = UserInputForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect ('success')
+            return render(request, 'task3/input_form.html', {'form': form})
